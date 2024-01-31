6ac7d3d90980edffa656c14b545433da1625e1d0
horiodino <holiodin@gmail.com>
2024-01-31 22:52:52 +0530

    config

---
+++ init.lua
@@ -0,0 +1,8 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+vim.cmd([[ hi Normal guibg=NONE ctermbg=NONE ]])
+vim.cmd([[ hi NonText guibg=NONE ctermbg=NONE ]])
+vim.cmd([[ hi VertSplit guibg=NONE ctermbg=NONE ]])
+vim.cmd([[ hi StatusLine guibg=NONE ctermbg=NONE ]])
+vim.cmd([[ hi StatusLineNC guibg=NONE ctermbg=NONE ]])
